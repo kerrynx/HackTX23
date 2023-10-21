a65eba657ef946b21b315781f5197be66e74d001
kerrynx <kerrynxu@utexas.edu>
2023-10-21 14:36:29 -0500

    fixed colors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Welcome to
+        </h1>
+        <h1>
+          Wedbud
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to
-          Wedbud
+          Get Started
         </p>
       </header>
     </div>
